85ff7626ce779066351cc7d997fb3423047888a3
Ian Andersen <ianderse@mac.com>
2014-10-07 07:53:41 -0600

    initial commit

---
+++ catify.gemspec
@@ -0,0 +1,14 @@
+% cat hola.gemspec
+Gem::Specification.new do |s|
+  s.name        = 'catify'
+  s.version     = '1.0'
+  s.date        = '2014-10-07'
+  s.summary     = "Catify"
+  s.description = "Cat-pun-ify any phrase"
+  s.authors     = ["Ian Andersen"]
+  s.email       = 'ianderse@mac.com'
+  s.files       = ["lib/catify.rb"]
+  s.homepage    =
+    'http://www.github.com/ianderse/catify'
+  s.license       = 'MIT'
+end
---
+++ lib/catify.rb
@@ -0,0 +1,53 @@
+CONVERT_HASH = {
+  'awesome' => 'pawsome',
+	'for' => 'fur',
+  'perfect' => 'purrfect',
+  'now' => 'meow',
+  'kidding' => 'kitten',
+  'awful' => 'clawful',
+  'positive' => 'pawsitive',
+  'personal' => 'purrsonal',
+  'persuasion' => 'purrsuasion',
+  'persuade' => 'purrsuade',
+  'fortunate' => 'furrtunate',
+  'unfortunate' => 'unfurrtunate',
+  'prefer' => 'pawfer',
+  'forever' => 'furever',
+  'lying' => 'lion',
+  'hysterical' => 'hissterical',
+  'pardon' => 'pawdon',
+  'feeling' => 'feline',
+  'clever' => 'clawver',
+  'tell' => 'tail',
+  'familiar' => 'furmiliar',
+  'talent' => 'tailent',
+  'mountain' => 'meowntain',
+  'attitude' => 'cattitude',
+  'inferior' => 'infurior',
+  'musician' => 'meowsician',
+  'tale' => 'tail',
+  'because' => 'be-claws',
+  'very' => 'furry',
+  'portable' => 'pawtable',
+  'you' => 'mew',
+  'got' => 'cat',
+  'please' => 'paw-lease',
+  'sorry' => 'paw-y',
+  'appalling' => 'apawling',
+  'possibly' => 'pawsibly',
+  'possibility' => 'pawsibility',
+}
+
+class Catify
+  def self.it(phrase)
+    phrase.downcase.split(' ').map do |word|
+      if CONVERT_HASH.include?(word)
+        CONVERT_HASH[word]
+      else
+        word
+      end
+    end.join(' ').capitalize
+  end
+end
+
+puts Catify.it("Jeff, I know this is possibly the most appalling thing I have ever done, but I hope you can please pardon my unfortunate attitude because this is awesome")
